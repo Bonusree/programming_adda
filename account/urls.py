1b728cb4e406e174a599ae0a8233759ce8c577b6
Mestu Paul <paulmestu@gmail.com>
2023-03-20 16:05:38 +0600

    update account urls

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,6 @@ from account import views
 
 urlpatterns = [
     path('register/',views.register),
-    path('login', views.login, name='login'),
-    path('home', views.home, name="home")
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name="home")
 ]
