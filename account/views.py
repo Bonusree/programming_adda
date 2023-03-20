e05934dfff3da3e680a738848a84fd99c3f203ca
Mestu Paul <paulmestu@gmail.com>
2023-03-20 16:05:22 +0600

    update account views

--- account/views.py
+++ account/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+
 @login_required(login_url='home')
 def register(request):
-    return render(request, 'register.html')
+    if request.method=="POST":
+        # if registered successfully render to home
+        # else show error message and render to register
+        pass
+    else:
+        return render(request, 'register.html')
 
 def login(request):
     if request.method=="POST":
@@ -15,22 +21,23 @@ def login(request):
         c_password=request.POST.get("c_password")
         if password!=c_password:
             msg="password doesn't matched"
-            return render(request, 'register.html', {'msg':msg})
+            return render(request, 'login.html', {'msg':msg})
         my_user=User.objects.create_user(name, email, password)
         my_user.save()
-        return render(request, 'login.html')
+        return render(request, 'home.html')
     else:
-        return render(request, 'register.html')
+        return render(request, 'login.html')
 
 def home(request):
-    if request.method=="POST":
-        name=request.POST.get("name")
-        password=request.POST.get("password")
-        user=authenticate(request,username=name,password=password)
-        if user is not None:
-            return HttpResponse("you are not registered")
-        else:
-            return HttpResponse("welcome")
-    else:
-        return render(request, 'login.html')
+    return render(request, 'home.html')
+    # if request.method=="POST":
+    #     name=request.POST.get("name")
+    #     password=request.POST.get("password")
+    #     user=authenticate(request,username=name,password=password)
+    #     if user is not None:
+    #         return HttpResponse("you are not registered")
+    #     else:
+    #         return HttpResponse("welcome")
+    # else:
+    #     return render(request, 'login.html')
         
