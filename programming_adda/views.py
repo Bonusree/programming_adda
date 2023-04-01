74d2577b1c20b59d5b597c40e99a6042803eb7ef
Bonusree <bonusreedatta820@gmail.com>
2023-04-02 01:07:47 +0600

    problems.html

--- programming_adda/views.py
+++ programming_adda/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,HttpResponse,redirect
 def login(request):
     return render(request,'login.html')
 
-def home_view(request):
-    print('hello my view')
-    return render(request,'home.html')
-
+def home(request):
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        user=authenticate(request,emailaddress=email,password=password)
+        print(user)
+        if user is not None:
+            return render(request, 'home.html')
+            
+        else:
+            return HttpResponse("you are not registered")
+    else:
+        return render(request, 'home.html')
 
 def register(request):
     return render(request,'register.html')
 
 def addproblem(request):
     return render(request,'addproblem.html')
+
+def problems(request):
+    return render(request,'problems.html')
