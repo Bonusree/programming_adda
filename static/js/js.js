44d42e1d98ba30a0afd5d24e440283959ccd5cdc
Mestu Paul <paulmestu@gmail.com>
2023-04-11 16:34:26 +0600

    use block system and extend base file

--- static/js/js.js
+++ static/js/js.js
@@ -12,13 +12,4 @@ $(document).ready(function() {
         cpassword.attr("type", type);
         this.classList.toggle("bi-eye");
     });
-      // Add click event handler to all menu links
-    $('.nav-link').on('click', function() {
-        console.log('Clicked link:', this);
-        // Remove the active class from the currently active link (if there is one)
-        $('.nav-link.active').removeClass('active');
-        
-        // Add the active class to the clicked link
-        $(this).addClass('active');
-    });
 });
