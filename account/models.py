7b82d190a4f8fd6012efe3684d4e3b0094774e25
Mestu Paul <paulmestu@gmail.com>
2023-04-14 21:37:34 +0600

    configure mongodb

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(primary_key=True,max_length=50)
+    password = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
