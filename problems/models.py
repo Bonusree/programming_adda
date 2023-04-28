8ccb6673128ac46255b66a1109e28c5f6fe75696
Mestu Paul <paulmestu@gmail.com>
2023-04-28 14:40:54 +0600

    change problem model

--- problems/models.py
+++ problems/models.py
@@ -4,14 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=255)
+    valid = models.BooleanField(default=False)
     
+    def __str__(self):
+        return self.name
+
+class Judge(models.Model):
+    name = models.CharField(max_length=255)
+    valid = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
     
 class Problem(models.Model):
     name = models.CharField(max_length=255)
     link = models.URLField()
-    judge = models.CharField(max_length=255)
+    judge = models.ForeignKey(Judge, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
