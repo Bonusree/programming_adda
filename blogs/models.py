b2b109e71cce0115020fc4ae3ad0f97caf16b98a
Mestu Paul <paulmestu@gmail.com>
2023-05-01 00:07:59 +0600

    create blogs model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+from problems.models import Problem
+
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    description = models.TextField()
+    related_problem = models.ManyToManyField(Problem)
+    additional_comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
