f30c79d176f4b443367def66ecda3db6bef43525
Mestu Paul <paulmestu@gmail.com>
2023-05-01 11:50:23 +0600

    update blogs view and url

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,4 +4,5 @@ from blogs import views
 
 urlpatterns = [
     path('blogs/',views.blogs, name='blogs'),
+    path('add-blogs/',views.add_blogs,name='add_blogs')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
-# @login_required
+@login_required
 def blogs(request):
     if request.method=="POST":
         pass
     else:
         return render(request, 'blogs.html')
+
+def add_blogs(request):
+    if request.method=="POST":
+        pass
+    else:
+        return render(request, 'add_blogs.html')
