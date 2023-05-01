11dec3c9fd9a50c133178e33355aa6d6a13d6b71
Mestu Paul <paulmestu@gmail.com>
2023-05-02 01:15:47 +0600

    remove everything from editorials

--- editorials/models.py
+++ editorials/models.py
@@ -6,12 +6,3 @@ from django.contrib.auth.models import User
 from problems.models import Tag, Judge
 
 
-class Editorials(models.Model):
-    name = models.CharField(max_length=255)
-    link = models.URLField()
-    judge = models.ForeignKey(Judge, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag)
-    tutorial = models.TextField()
-    code = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
