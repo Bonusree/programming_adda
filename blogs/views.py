7b8fa1d725a8b4f4813b377d0b42c190db2f4cc7
Mestu Paul <paulmestu@gmail.com>
2023-05-03 00:33:05 +0600

    add new blog and show it

--- blogs/views.py
+++ blogs/views.py
@@ -4,15 +4,48 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 
-@login_required
+from .models import Blogs
+
 def blogs(request):
+    context = {"type":'','message':''}
     if request.method=="POST":
         pass
     else:
-        return render(request, 'blogs.html')
+        all_blogs = Blogs.objects.all()
+        context = {'blogs':[]}
+        for b in all_blogs:
+            blog = {'name':b.title,'link':b.link,
+                    'description':b.description,'comment':b.additional_comment,
+                    'contributor':b.user,'blog_id':b.id}
+            
+            context['blogs'].append(blog)
+        print("context-----------\n",context)
+        return render(request, 'blogs.html',context)
 
+@login_required
 def add_blogs(request):
+    print("-------request method------")
+    print(request.method)
     if request.method=="POST":
-        pass
+        blog_name = request.POST.get("blog_name")
+        blog_link = request.POST.get("blog_link")
+        description = request.POST.get("description")
+        comment = request.POST.get("comment")
+        user = request.user
+        
+        # print(blog_name)
+        # print(blog_link)
+        # print(description)
+        # print(comment)
+        context = {'type':'success','message':'Thanks for your contribution'}
+        try:
+            new_blog = Blogs(title=blog_name,link=blog_link,description=description,additional_comment=comment,user=user)
+            new_blog.save()
+        except Exception as e:
+            context["type"]='error'
+            context["message"]=f'Error with database {e}'
+            print("error :",e)
+        return render(request,'add_blogs.html',context)
+            
     else:
         return render(request, 'add_blogs.html')
--- temp.py
+++ temp.py
@@ -1,7 +1,22 @@
-import os
-os.system("git rm -r --cached ./account/__pycache__")
-os.system("git rm -r --cached ./blogs/__pycache__")
-os.system("git rm -r --cached ./contestants/__pycache__")
-os.system("git rm -r --cached ./editorials/__pycache__")
-os.system("git rm -r --cached ./problems/__pycache__")
-os.system("git rm -r --cached ./programming_adda/__pycache__")
+from datetime import date
+
+current_date = date(2023,1,1)
+
+
+feb_2020 = date(2020, 1, 1)
+current_year = current_date.year
+if current_date.month<2:
+    current_year-=1
+    
+current_feb = date(current_year,2,1)
+
+
+# Calculate the difference between the current month and February 2020
+month_diff_current = (current_date.year - feb_2020.year) * 12 + (current_date.month - feb_2020.month)+1
+month_diff_feb = (current_feb.year - feb_2020.year) * 12 + (current_feb.month - feb_2020.month)
+
+output =[i for i in range(month_diff_current,month_diff_feb,-1)]
+print(output)
+
+
+
