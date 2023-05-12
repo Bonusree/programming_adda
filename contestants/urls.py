3c718bf24a5297b1d6d0570e91eb67184c293f43
Mestu Paul <paulmestu@gmail.com>
2023-05-12 18:31:11 +0600

    update contestant table

--- contestants/urls.py
+++ contestants/urls.py
@@ -5,4 +5,5 @@ from contestants import views
 urlpatterns = [
     path('contestants/',views.contestants, name='contestants'),
     path('profile/',views.profile, name='profile'),
+    path('contestants/single-user-info/',views.single_user_info, name='single_user_info'),
 ]
--- contestants/views.py
+++ contestants/views.py
@@ -1,16 +1,39 @@
 from django.shortcuts import render
-from django.shortcuts import render,HttpResponse,redirect
+from django.db.models import Count,Q
+
+from problems.models import Problem
+from blogs.models import Blogs
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
 
 # @login_required
 def contestants(request):
     if request.method=="POST":
         pass
     else:
-        return render(request, 'contestants.html')
+        users = User.objects.all()
+        contestants_info = []
+        for user in users:
+            num_problems = Problem.objects.filter(user=user).count()
+            num_blogs = Blogs.objects.filter(user=user).count()
+            contestants_info.append({'username':user.username,'problems':num_problems,'editorials':0,'blogs':num_blogs,'others':0})
+        context = {'contestants_info':contestants_info}
+        return render(request, 'contestants.html',context=context)
 
 def profile(request):
-   
     return render(request, 'profile.html')
+
+def single_user_info(request):
+    username = request.POST.get('searchText')
+    ctx = {}
+    try:
+        user = User.objects.get(username=username)
+        contestants_info = []
+        num_problems = Problem.objects.filter(user=user).count()
+        num_blogs = Blogs.objects.filter(user=user).count()
+        contestants_info.append({'username':username,'problems':num_problems,'editorials':0,'blogs':num_blogs,'others':0})
+        ctx['contestants_info']=contestants_info
+    except User.DoesNotExist:
+        ctx['type'] = 'info'
+        ctx['message'] = f'No data for {username}'
+    return render(request, 'contestants.html',context=ctx)
+        
